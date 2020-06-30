c20faf6d2d89a6a4f43ab278294b6b075f51874a
kalavhan <josue.brigido1@gmail.com>
2020-06-30 15:47:18 -0600

    Add linter files

--- src/index.js
+++ src/index.js
@@ -2,25 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import PropTypes from 'prop-types';
+
 const Big = require('big.js');
 
-class Num extends React.Component {
-  render() {
-    return (
-    <h2>{this.props.value}</h2>
-    )
-  }
-}
-
-Num.defaultProps = {
-  value: 0
-};
+const Num = ({ value }) => <h2>{value}</h2>;
 
 Num.propTypes = {
-  value: PropTypes.number.isRequired
+  value: PropTypes.number.isRequired,
 };
 
 ReactDOM.render(
-    <Num value={Number(Big(0.4).minus(0.1))}/>,
-  document.getElementById('root')
+  <Num value={Number(Big(0.4).minus(0.1))} />,
+  document.getElementById('root'),
 );
