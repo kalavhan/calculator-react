ebd8f368df1f4c6f082557c611a1eb689a4b2a86
kalavhan <josue.brigido1@gmail.com>
2020-07-01 13:38:00 -0600

    Add style to the display

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropType from 'prop-types';
 
-const Button = ({ name }) => <button type="button">{ name }</button>;
+const Button = ({ name, elmntClass }) => <button type="button" className={`calcButton ${elmntClass}`}>{ name }</button>;
 
 Button.propTypes = {
   name: PropType.string.isRequired,
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,33 +4,90 @@ import Button from './Button';
 const ButtonPanel = () => (
   <div className="buttonPanel">
     <div className="buttonPanel-row">
-      <Button name="AC" />
-      <Button name="+/-" />
-      <Button name="%" />
-      <Button name="÷" />
+      <Button
+        name={"AC"}
+        elmntClass={"w25"}
+      />
+      <Button
+        name={"+/-"}
+        elmntClass={"w25"}
+      />
+      <Button
+        name={"%"}
+        elmntClass={"w25"}
+      />
+      <Button
+        name={"÷"}
+        elmntClass={"w25"}
+      />
     </div>
     <div className="buttonPanel-row">
-      <Button name="7" />
-      <Button name="8" />
-      <Button name="9" />
-      <Button name="X" />
+      <Button
+        name={"7"}
+        elmntClass={"w25"}
+      />
+      <Button
+        name={"8"}
+        elmntClass={"w25"}
+      />
+      <Button
+        name={"9"}
+        elmntClass={"w25"}
+      />
+      <Button
+        name={"X"}
+        elmntClass={"w25"}
+      />
     </div>
     <div className="buttonPanel-row">
-      <Button name="4" />
-      <Button name="5" />
-      <Button name="6" />
-      <Button name="-" />
+      <Button
+        name={"4"}
+        elmntClass={"w25"}
+      />
+      <Button
+        name={"5"}
+        elmntClass={"w25"}
+      />
+      <Button
+        name={"6"}
+        elmntClass={"w25"}
+      />
+      <Button
+        name={"-"}
+        elmntClass={"w25"}
+      />
     </div>
     <div className="buttonPanel-row">
-      <Button name="1" />
-      <Button name="2" />
-      <Button name="3" />
-      <Button name="+" />
+      <Button
+        name={"1"}
+        elmntClass={"w25"}
+      />
+      <Button
+        name={"2"}
+        elmntClass={"w25"}
+      />
+      <Button
+        name={"3"}
+        elmntClass={"w25"}
+      />
+      <Button
+        name={"+"}
+        elmntClass={"w25"}
+      />
     </div>
     <div className="buttonPanel-row">
-      <Button name="0" />
-      <Button name="." />
-      <Button name="=" />
+      <Button
+        name={"0"}
+        elmntClass={"w50"}
+      />
+      <Button
+        name={"."}
+        elmntClass={"w25"}
+      />
+      <Button
+        name={"="}
+        elmntClass={"w25"}
+      />
     </div>
   </div>
 );
