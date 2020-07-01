be871bb4ccbcccb4b7527923f25abf0c38e09225
kalavhan <josue.brigido1@gmail.com>
2020-07-01 09:36:26 -0600

    Add button component

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Display from './Display';
+import ButtonPanel from './ButtonPannerl';
+
+const App = () => {
+  return (
+    <div>
+      <Display />
+      <ButtonPanel />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropType from 'prop-type';
+
+const Button = ({ name }) => <button type="button">{ name }</button>;
+
+Button.propTypes = {
+  name: PropType.string.isRequired,
+};
